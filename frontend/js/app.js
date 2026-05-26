c6dcdaa1999ae00c0de1cada7c25799562b070c6
Kevin Baraka <kevinwisebaraka@gmail.com>
2026-05-26 08:57:48 +0300

    Update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,1 +1,27 @@
-console.log("SmartShop Website Loaded");
+// CART ARRAY
+
+let cart = [];
+
+// ADD TO CART FUNCTION
+
+function addToCart(productName, productPrice){
+
+    // CREATE PRODUCT OBJECT
+
+    const product = {
+        name: productName,
+        price: productPrice
+    };
+
+    // PUSH PRODUCT INTO CART ARRAY
+
+    cart.push(product);
+
+    // SHOW MESSAGE
+
+    alert(productName + " added to cart!");
+
+    // CHECK CART IN CONSOLE
+
+    console.log(cart);
+}
